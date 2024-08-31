fc374ae1df2774e6cfc15f4856f199781e338921
Marcelo Augusto <102921581+MarceloRyujji@users.noreply.github.com>
2024-08-31 12:29:06 +1000

    Update serviceRoutes.js

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import ServiceController from "../controllers/servicesController.js";
+import checkApiKey from "../config/authorized.js";
 
 const routes = express.Router();
 
-routes.get("/service", ServiceController.ListService);
-routes.get("/service/:id", ServiceController.ListServiceById);
-routes.post("/service", ServiceController.RegisterService);
-routes.put("/service/:id", ServiceController.updateService);
-routes.delete("/service/:id", ServiceController.deleteService);
+routes.get("/service", checkApiKey, ServiceController.ListService);
+routes.get("/service/:id", checkApiKey, ServiceController.ListServiceById);
+routes.post("/service", checkApiKey, ServiceController.RegisterService);
+routes.put("/service/:id", checkApiKey, ServiceController.updateService);
+routes.delete("/service/:id",  checkApiKey, ServiceController.deleteService);
 
 export default routes;
