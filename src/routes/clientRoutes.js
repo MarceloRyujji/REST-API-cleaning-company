6a3b2089fe9679a28d55a6cc7e8a660e9401e756
Marcelo Augusto <102921581+MarceloRyujji@users.noreply.github.com>
2024-08-31 12:28:12 +1000

    Update clientRoutes.js
    include API key

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import ClientController from "../controllers/clientsController.js";
+import checkApiKey from "../config/authorized.js";
 
 const routes = express.Router();
 
-routes.get("/clients", ClientController.ListClients);
-routes.get("/clients/:id", ClientController.ListClientById);
-routes.post("/clients", ClientController.RegisterCustomers);
-routes.put("/clients/:id", ClientController.updateClient);
-routes.delete("/clients/:id", ClientController.deleteClient);
+routes.get("/clients", checkApiKey, ClientController.ListClients);
+routes.get("/clients/:id",checkApiKey, ClientController.ListClientById);
+routes.post("/clients", checkApiKey, ClientController.RegisterCustomers);
+routes.put("/clients/:id", checkApiKey, ClientController.updateClient);
+routes.delete("/clients/:id", checkApiKey, ClientController.deleteClient);
 
 export default routes;
