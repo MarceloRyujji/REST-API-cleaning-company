4291e59ade3d116da28e3294a5bfdd1844f12dd0
Marcelo Augusto <102921581+MarceloRyujji@users.noreply.github.com>
2024-08-31 12:28:45 +1000

    Update ordersRoutes.js
    include API key

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import OrderController from "../controllers/ordersController.js";
+import checkApiKey from "../config/authorized.js";
 
 const routes = express.Router();
 
-routes.get("/order", OrderController.ListOrders);
-routes.get("/order/:id", OrderController.ListOrderById);
-routes.post("/order", OrderController.RegisterOrder);
-routes.put("/order/:id", OrderController.updateOrder);
-routes.delete("/order/:id", OrderController.deleteOrder);
+routes.get("/order", checkApiKey, OrderController.ListOrders);
+routes.get("/order/:id", checkApiKey, OrderController.ListOrderById);
+routes.post("/order",checkApiKey, OrderController.RegisterOrder);
+routes.put("/order/:id",checkApiKey, OrderController.updateOrder);
+routes.delete("/order/:id",checkApiKey, OrderController.deleteOrder);
 
 export default routes;
